699d28c6157518c4493899e2eeaa8af08346e5e7
yhql <victor_servant@hotmail.com>
2022-09-26 10:43:25 +0200

    publish

--- src/lib.rs
+++ src/lib.rs
@@ -13,8 +13,9 @@ pub fn include_gif(input: TokenStream) -> TokenStream {
     decoder.set_color_output(gif::ColorOutput::Indexed);
     let file = File::open(filename.value()).unwrap();
     let mut decoder = decoder.read_info(file).unwrap();
-    let frame = decoder.read_next_frame().unwrap().unwrap();
 
+    let frame = decoder.read_next_frame().unwrap().unwrap().clone();
+    let palette = decoder.palette().unwrap();
     let dimensions = frame.width * frame.height;
     let (size, remainder) = ((dimensions / 8) as usize, (dimensions % 8) as usize);
     
@@ -22,20 +23,20 @@ pub fn include_gif(input: TokenStream) -> TokenStream {
     for i in 0..size {
         let mut byte = 0;
         for j in 0..8 {
-            let color = (frame.buffer[8*i + j] != 0) as u8;
+            let color = (palette[frame.buffer[8*i + j] as usize * 3] != 0) as u8;
             byte |= color << j;
         }
         packed.push(byte);
     }
     let mut byte = 0; 
     for j in 0..remainder {
-        let color = (frame.buffer[8*size + j] != 0) as u8;
+        let color = (palette[frame.buffer[8*size + j] as usize * 3] != 0) as u8;
         byte |= color << j;
     }
     packed.push(byte);
 
     let mut b = Vec::new();
-    write!(&mut b, "({:?}, {}, {})", packed, frame.width, frame.height).unwrap();
+    write!(&mut b, "(&{:?}, {}, {})", packed, frame.width, frame.height).unwrap();
     let a = std::str::from_utf8(&b).unwrap();
 
     a.parse().unwrap()
